22d8b08b9c9f3a982226e6c37ede6726a949cbde
Nikky <nikkyshah18@gmail.com>
2025-04-04 15:32:25 +0530

    Update fill.groovy

--- vars/fill.groovy
+++ vars/fill.groovy
@@ -2,3 +2,15 @@ def gitdown(repo)
 {
   git "https://github.com/nikky1804/${repo}.git"
 }
+def contbuilt()
+{
+  sh 'mvn package'
+}
+def deploy(jobname,ipaddress,filename)
+{
+  sh "scp var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:var/lib/tomcat10/webapp/${filename}.war"
+}
+def test()
+{
+  sh "java -jar var/lib/tomcat10/webapp/${filename}.jar
+}
