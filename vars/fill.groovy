944acaa52b020491ac322d9fd082afbe0be33755
Nikky <nikkyshah18@gmail.com>
2025-04-04 17:54:27 +0530

    Update fill.groovy

--- vars/fill.groovy
+++ vars/fill.groovy
@@ -8,7 +8,7 @@ def contbuilt()
 }
 def deploy(jobname,ipaddress,filename)
 {
-  sh "scp var/lib/jenkins/workspace/${jobname}/webapps/target/webapp.war ubuntu@${ipaddress}:var/lib/tomcat10/webapp/${filename}.war"
+  sh "scp var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:var/lib/tomcat10/webapps/${filename}.war"
 }
 def test()
 {
