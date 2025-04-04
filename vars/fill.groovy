6747f0ca75e79a9143d129da57c81f4c40dbbfb8
Nikky <nikkyshah18@gmail.com>
2025-04-04 19:14:28 +0530

    fill.groovy

--- vars/fill.groovy
+++ vars/fill.groovy
@@ -10,7 +10,7 @@ def deploy(jobname,ipaddress,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${context}.war"
 }
-def test()
+def test(jobname)
 {
-  sh "java -jar var/lib/tomcat10/webapp/${filename}.jar"
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
